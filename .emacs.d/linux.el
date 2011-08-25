6afab9519f681175097abc0c00723b62c2d9e6e3
Graham Taylor <gwtaylor@gmail.com>
2011-08-25 13:05:47 -0400

    Working on linux - but not sun machines
    Also haven't re-tested OSX

---
+++ .emacs.d/linux.el
@@ -0,0 +1,51 @@
+;; Linux-specific settings
+
+;Matlab mode stuff
+;First, set path
+(setq load-path (cons "~/elisp/matlab-emacs" load-path))
+
+(autoload 'matlab-mode "matlab" "Enter MATLAB mode." t)
+(setq auto-mode-alist (cons '("\\.m\\'" . matlab-mode) auto-mode-alist))
+(autoload 'matlab-shell "matlab" "Interactive MATLAB mode." t)
+
+(setq matlab-indent-function-body t)  ; if you want function bodies indented
+(setq matlab-verify-on-save-flag nil) ; turn off auto-verify on save
+
+(defun my-matlab-mode-hook ()
+  (define-key matlab-mode-map [(control c)(!)] 'matlab-shell) ; like python
+  (setq fill-column 76)              ; where auto-fill should wrap
+  (setq matlab-indent-level 2))      ; Set matlab indentation
+(add-hook 'matlab-mode-hook 'my-matlab-mode-hook)
+
+(defun my-matlab-shell-mode-hook ()
+  '())
+(add-hook 'matlab-shell-mode-hook 'my-matlab-shell-mode-hook)
+
+;; disable startup message
+(setq inhibit-startup-message t)
+
+;; Show column number at bottom of screen
+(column-number-mode 1)
+
+;; Pgup/dn will return exactly to the starting point.
+(setq scroll-preserve-screen-position 1)
+
+;; scroll just one line when hitting the bottom of the window
+(setq scroll-step 1)
+(setq scroll-conservatively 1)
+
+;; format the title-bar to always include the buffer name
+(setq frame-title-format "emacs - %b")
+
+;; turn on word wrapping in text mode
+(add-hook 'text-mode-hook 'turn-on-auto-fill)
+
+;; replace highlighted text with what I type rather than just
+;; inserting at a point
+(delete-selection-mode t)
+
+; get rid of "yes or no" and replace with "y or n":
+(fset 'yes-or-no-p 'y-or-n-p)
+
+; to edit files from within matlab
+(server-start)
