560f0657bea33f46fbef811eb0c81f13f55e6f43
Graham Taylor <gwtaylor@gmail.com>
2011-11-05 18:31:47 -0400

    This should be a fix for new frames having the wrong color-theme (light)

--- .emacs.d/appearance.el
+++ .emacs.d/appearance.el
@@ -12,6 +12,35 @@
 ;))
 
 (load-file "~/src/solarized/emacs-colors-solarized/color-theme-solarized.el")
-(color-theme-solarized-dark)
+;(color-theme-solarized-dark)
+
+;; Got this from: http://emacs-fu.blogspot.com/2009/03/color-theming.html
+;; (one of the comments)
+;; When Aquamacs opens a new frame (e.g. for help) I get light-background solarized
+;; So introducing a hook on make-frame, it sets it to black
+;; This will also set a different theme for non-windowing frames
+;; Color themes
+(require 'color-theme)
+;; hook: test win sys to rerun ctheme
+(defun test-win-sys(frame)
+;; must be current for local ctheme
+ (select-frame frame)
+ ;; test winsystem
+ (if (window-system frame)
+  (color-theme-solarized-dark)
+  (color-theme-tty-dark)
+ )
+)
+;; hook on after-make-frame-functions
+(add-hook 'after-make-frame-functions 'test-win-sys)
+
+;; default start - if non daemon
+(let ((color-theme-is-global nil))
+ (if (window-system)
+  (color-theme-solarized-dark)
+  (color-theme-tty-dark)
+ )
+)
+
 
 (provide 'appearance)
